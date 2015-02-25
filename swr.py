796494173c88558e3add62caf89f0dfc8709c21f
michielbrink <michielpa@gmail.com>
2015-02-25 16:25:26 +0100

    let the buttons change the frequency

--- swr.py
+++ swr.py
@@ -10,14 +10,14 @@ from matplotlib.widgets import Button
 buttonY = 0.05
 buttonHight = 0.1
 
-buttonList = [['1.81','1.88'], ['3.50','3.80'], ['7.00','7.10'], ['10.10','10.150'], ['14.00','14.35'], ['18.06','18-17'], ['21.00','21.45'], ['24.89','24.99'], ['28.00','29.70'], ['144','146']]
+buttonList = [['1.81','1.88'], ['3.50','3.80'], ['7.00','7.10'], ['10.10','10.150'], ['14.00','14.35'], ['18.06','18.17'], ['21.00','21.45'], ['24.89','24.99'], ['28.00','29.70'], ['144','146']]
 
 buttonPos = [0] * len(buttonList)
 buttonObject = [0] * len(buttonList)
 buttonWeight = 1 / len(buttonList)
 
 #simulate data
-x = np.arange(0.0, 5.0, 0.01)
+x = np.arange(0.0, 30.0, 0.01)
 y = np.sin(2*np.pi*x) + 0.5*np.random.randn(len(x))
 
 #add figure
@@ -38,11 +38,11 @@ ax.set_title('Antenna analyzer by PB4M')
 
 #button event
 class Click_class:
-    def __init__(self, index):
-        self.index = index
+    def __init__(self, minFreq, maxFreq):
+        self.minFreq = minFreq
+        self.maxFreq = maxFreq
     def Click(self, event):
-        print self.index
-        ax2.set_xlim(1, 2)
+        ax2.set_xlim(self.minFreq, self.maxFreq)
         plt.draw()
 
 
@@ -51,7 +51,7 @@ for index, string in enumerate(buttonList):
     buttonX = ( buttonWeight * index ) 
     buttonPos[index] = plt.axes([buttonX, buttonY, buttonWeight, buttonHight])
     buttonObject[index] = Button(buttonPos[index], string[0] + "-" + string[1])
-    buttonObject[index].on_clicked(Click_class(index).Click)
+    buttonObject[index].on_clicked(Click_class(float(string[0]), float(string[1])).Click)
 
 #show screen
 plt.show()
